70addcfc644f697df7811119a923e45720de423f
Aleksandr Daniloff <aleksandr.daniloff@parkingmap.fr>
2019-03-04 15:31:52 +0100

    [~] -> Converter code improvement

--- src/Service/Converter.php
+++ src/Service/Converter.php
@@ -4,161 +4,112 @@ namespace App\Service;
 
 class Converter
 {
+    private static $number;
+
+    private static $numerals = [
+        0 => "",
+        1 => "I",
+        2 => "II",
+        3 => "III",
+        4 => "IV",
+        5 => "V",
+        6 => "VI",
+        7 => "VII",
+        8 => "VIII",
+        9 => "IX",
+    ];
+
+    private static $decimals = [
+        1 => "X",
+        2 => "XX",
+        3 => "XXX",
+        4 => "XL",
+        5 => "L",
+        6 => "LX",
+        7 => "LXX",
+        8 => "LXXX",
+        9 => "XC",
+    ];
+
+    private static $hundredths = [
+        1 => "C",
+        2 => "CC",
+        3 => "CCC",
+        4 => "CD",
+        5 => "D",
+        6 => "DC",
+        7 => "DCC",
+        8 => "DCCC",
+        9 => "CM",
+    ];
+
+    private static $thousands = [
+        1 => "M",
+        2 => "MM",
+        3 => "MMM",
+    ];
+    
     public static function convert(int $number): string
     {
-        if (!is_int($number)) {
-            throw new \LogicException("Invalid argument number: not supported");
+        self::$number = $number;
+        self::checkNumberValidity();
+
+        $romanNumeral = "";
+
+        if (self::$number > 999) {
+            $romanNumeral .= self::convertThousandDigit();
         }
 
-        if (is_int($number) and $number < 1) {
+        if (self::$number > 99) {
+            $romanNumeral .= self::convertHundredthDigit();
+        }
+
+        if (self::$number > 9) {
+            $romanNumeral .= self::convertDecimal();
+        }
+
+        $romanNumeral .= self::convertNumeral();
+
+        return $romanNumeral;
+    }
+
+    private static function checkNumberValidity()
+    {
+        if (self::$number < 1) {
             throw new \LogicException("Invalid argument number: cant be less than 1");
         }
 
-        if (is_int($number) and $number > 3999) {
+        if (self::$number > 3999) {
             throw new \LogicException("Invalid argument number: cant be greater than 3999");
         }
-
-        if ($number < 10) {
-            return self::convertSingleDigit($number);
-        }
-        if ($number < 100) {
-            return self::convertDoubleDigit($number);
-        }
-        if ($number < 1000) {
-            return self::convertTripleDigit($number);
-        }
-        if ($number < 4000) {
-            return self::convertQuadDigit($number);
-        }
-
-        throw new \LogicException("Invalid argument number: not detected");
     }
 
-    private static function convertSingleDigit($number): ?string
+    private static function convertThousandDigit(): ?string
     {
-        switch ($number) {
-            case 1:
-                return "I";
-            case 2:
-                return "II";
-            case 3:
-                return "III";
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            case 6:
-                return "VI";
-            case 7:
-                return "VII";
-            case 8:
-                return "VIII";
-            case 9:
-                return "IX";
-        }
+        $thousandNumber = (int)floor(self::$number / 1000);
+        self::$number -= $thousandNumber * 1000;
 
-        return '';
+        return self::$thousands[$thousandNumber];
     }
 
-    private static function convertDoubleDigit($number): ?string
+    private static function convertHundredthDigit(): ?string
     {
-        $value = "";
-        $decDigit = floor($number / 10);
+        $hundredthNumber = (int)floor(self::$number / 100);
+        self::$number -= $hundredthNumber * 100;
 
-        switch ($decDigit) {
-            case 1:
-                $value = "X";
-                break;
-            case 2:
-                $value = "XX";
-                break;
-            case 3:
-                $value = "XXX";
-                break;
-            case 4:
-                $value = "XL";
-                break;
-            case 5:
-                $value = "L";
-                break;
-            case 6:
-                $value = "LX";
-                break;
-            case 7:
-                $value = "LXX";
-                break;
-            case 8:
-                $value = "LXXX";
-                break;
-            case 9:
-                $value = "XC";
-                break;
-        }
-
-        $value = $value . self::convertSingleDigit($number - ($decDigit * 10));
-
-        return $value;
+        return self::$hundredths[$hundredthNumber];
     }
 
-    private static function convertTripleDigit($number): ?string
+    private static function convertDecimal(): ?string
     {
-        $value = "";
-        $centDigit = floor($number / 100);
+        $decimalNumber = (int)floor(self::$number / 10);
+        self::$number -= $decimalNumber * 10;
 
-        switch ($centDigit) {
-            case 1:
-                $value = "C";
-                break;
-            case 2:
-                $value = "CC";
-                break;
-            case 3:
-                $value = "CCC";
-                break;
-            case 4:
-                $value = "CD";
-                break;
-            case 5:
-                $value = "D";
-                break;
-            case 6:
-                $value = "DC";
-                break;
-            case 7:
-                $value = "DCC";
-                break;
-            case 8:
-                $value = "DCCC";
-                break;
-            case 9:
-                $value = "CM";
-                break;
-        }
-
-        $value = $value . self::convertDoubleDigit($number - ($centDigit * 100));
-
-        return $value;
+        return self::$decimals[$decimalNumber];
     }
 
-    private static function convertQuadDigit($number): ?string
+    private static function convertNumeral(): ?string
     {
-        $value = "";
-        $thouDigit = floor($number / 1000);
-
-        switch ($thouDigit) {
-            case 1:
-                $value = "M";
-                break;
-            case 2:
-                $value = "MM";
-                break;
-            case 3:
-                $value = "MMM";
-                break;
-        }
-
-        $value = $value . self::convertTripleDigit($number - ($thouDigit * 1000));
-
-        return $value;
+        return self::$numerals[self::$number];
     }
 }
